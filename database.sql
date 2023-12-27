f0043521013fbfab54d1ab40ceb5399a3ce0ac70
Sergei <reyangood@gmail.com>
2023-12-28 00:56:11 +0300

    add new table in database.sql

--- database.sql
+++ database.sql
@@ -1,6 +1,17 @@
-CREATE TABLE urls
+CREATE TABLE IF NOT EXISTS urls
 (
     id         bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name       varchar(255),
     created_at date
+);
+
+CREATE TABLE IF NOT EXISTS url_checks
+(
+    id          bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id      bigint,
+    status_code integer,
+    h1          varchar(255),
+    title       varchar(255),
+    description text,
+    created_at  date
 );
