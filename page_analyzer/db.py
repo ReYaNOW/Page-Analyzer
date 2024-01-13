3f3a5c0bd6799fe54b67d92b25aaa598684d65e1
Sergei <reyangood@gmail.com>
2024-01-13 07:43:03 +0300

    code refactoring in db.py

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,11 +1,15 @@
 import os
+from collections import namedtuple
 
 import psycopg2
-from dotenv import load_dotenv
 from psycopg2.extras import NamedTupleCursor
 
-load_dotenv('.env')
 DATABASE_URL = os.getenv('DATABASE_URL')
+
+_FETCH_TYPE_VALUES = ('one', 'all')
+FETCH_TYPES = namedtuple('FormatChoices', map(str.upper, _FETCH_TYPE_VALUES))(
+    *_FETCH_TYPE_VALUES
+)
 
 
 class Database:
@@ -13,70 +17,67 @@ class Database:
         if connect:
             self.conn = psycopg2.connect(DATABASE_URL)
         else:
-            self.conn = None
+            raise ConnectionError('Need to connect to db before execute query')
 
-    def _make_request(self, request, params=None, fetch_type=None):
-        if self.conn is None:
-            raise ConnectionError(
-                'Need to connect to db before making requests'
-            )
-
+    def _execute_query(self, request, params=None, fetch_type=None):
         with self.conn.cursor(cursor_factory=NamedTupleCursor) as curs:
             curs.execute(request, params)
 
             match fetch_type:
-                case 'all':
+                case FETCH_TYPES.ALL:
                     data = curs.fetchall()
-                case 'one':
+                case FETCH_TYPES.ONE:
                     data = curs.fetchone()
                 case _:
-                    data = None
+                    data = []
         return data
 
+    def _execute_query_with_fetch(self, request, params=None, fetch_type=None):
+        pass
+
     def get_all_urls(self):
-        return self._make_request(
+        return self._execute_query(
             'SELECT * FROM urls ORDER BY id DESC', fetch_type='all'
         )
 
     def get_url_by_name(self, url):
-        return self._make_request(
+        return self._execute_query(
             'SELECT * FROM urls WHERE name=%s', [url], fetch_type='one'
         )
 
     def get_url_by_id(self, id_):
-        return self._make_request(
+        return self._execute_query(
             'SELECT * FROM urls WHERE id=%s', [id_], fetch_type='one'
         )
 
     def add_new_url(self, url):
-        self._make_request(
-            'INSERT INTO urls (name) VALUES (%s)',
+        added_url = self._execute_query(
+            'INSERT INTO urls (name) VALUES (%s) RETURNING id',
             [url],
+            fetch_type='one',
         )
-        self.commit()
-        added_url = self._make_request(
-            'SELECT * FROM urls WHERE name=%s', [url], fetch_type='one'
-        )
+        self.conn.commit()
         return added_url.id
 
     def add_check(self, url_id, status_code, tags):
         h1, title, desc = tags['h1'], tags['title'], tags['desc']
-        self._make_request(
+        self._execute_query(
             'INSERT INTO url_checks '
             '(url_id, status_code, h1, title, description) '
             'VALUES (%s, %s, %s, %s, %s)',
             [url_id, status_code, h1, title, desc],
         )
+        self.conn.commit()
 
     def get_all_checks_for_url(self, url_id):
-        return self._make_request(
+        return self._execute_query(
             'SELECT * FROM url_checks WHERE url_id=%s ORDER BY id DESC',
             [url_id],
             fetch_type='all',
         )
 
     def get_urls_with_code(self):
-        return self._make_request(
+        return self._execute_query(
             'SELECT DISTINCT ON (urls.id) urls.id, name,  '
             'url_checks.created_at AS last_check, '
             'status_code AS last_code '
@@ -86,12 +87,5 @@ class Database:
             fetch_type='all',
         )
 
-    def connect(self):
-        self.conn = psycopg2.connect(DATABASE_URL)
-
-    def commit(self):
-        self.conn.commit()
-
     def close(self):
-        self.conn.commit()
         self.conn.close()
