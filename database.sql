578e3d0c928da8a8c08cbd41ea3a32dc0a4cf0e3
Sergei <reyangood@gmail.com>
2024-01-17 03:17:15 +0300

    change data type params in database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS urls
 (
     id         serial PRIMARY KEY,
-    name       varchar(255),
+    name       varchar(255) NOT NULL UNIQUE,
     created_at date DEFAULT CURRENT_DATE
 );
 
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS urls
 CREATE TABLE IF NOT EXISTS url_checks
 (
     id          serial PRIMARY KEY,
-    url_id      integer REFERENCES urls(id) ON DELETE CASCADE,
+    url_id      integer REFERENCES urls (id) ON DELETE CASCADE,
     status_code integer,
     h1          varchar(255),
     title       varchar(255),
